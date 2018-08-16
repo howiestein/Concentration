dee924fd373754d05c6a24465ff3aa2e9d87181b
Howard Stein <hss4@cornell.edu>
2018-08-15 21:04:37 -0400

    Add copyright notice

--- js/utilities.js
+++ js/utilities.js
@@ -136,12 +136,13 @@ function updateTimer(start) {
 // Function to generate a dated copyright notice
 // Code taken from https://answers.squarespace.com/questions/58787/how-to-display-current-year-for-copyright.html -->
 // TODO: in 2019, change this to be "© 2018-[CURRENT YEAR] Howard S. Stein"
-// TODO Change to create DOM text object and attach it as a child of the element passed as an argument
 function generateCopyright() {
   var date = new Date(),
       year = date.getFullYear(),
       text = "© " + year + " Howard S. Stein",
-      html = '<div class="text-align-center">' + text + '</div>';
+      html = document.createElement("div");
 
-   document.write(html);
+      html.textContent = text;
+
+      document.querySelector("footer").appendChild(html);
 }
